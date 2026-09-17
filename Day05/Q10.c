93be00779e5e6c5c1b89d118c47b37b528d5d87c
ishanvi2505 <Ishanvi.41238@stu.upes.ac.in>
2026-09-17 20:37:02 +0530

    Add program to convert seconds to HH:MM:SS format

---
+++ Day05/Q10.c
@@ -0,0 +1,25 @@
+/*
+Name: Ishanvi Pahil
+SAP ID: 590041238
+Date: 5 September 2026
+Question No: Q10
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int seconds, hours, minutes;
+
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    seconds = seconds % 3600;
+
+    minutes = seconds / 60;
+    seconds = seconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
