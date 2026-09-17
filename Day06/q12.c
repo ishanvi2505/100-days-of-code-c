4f1b27e3884f338055357b2100109d6104446aeb
ishanvi2505 <Ishanvi.41238@stu.upes.ac.in>
2026-09-17 20:40:34 +0530

    Add program to check integer sign using nested if-else
    This program checks if an input integer is positive, negative, or zero using nested if-else statements.

---
+++ Day06/q12.c
@@ -0,0 +1,33 @@
+/*
+Name: Ishanvi Pahil
+SAP ID: 590041238
+Date: 10 September 2026
+Question No: Q12
+
+Question:
+Write a program to input an integer and check whether
+it is positive, negative, or zero using nested if-else.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    if(n >= 0)
+    {
+        if(n == 0)
+            printf("Zero");
+        else
+            printf("Positive");
+    }
+    else
+    {
+        printf("Negative");
+    }
+
+    return 0;
+}
