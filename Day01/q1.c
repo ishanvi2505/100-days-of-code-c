456e31b5b9dec503ef01abc3c72620f5ed0d5c6b
ishanvi2505 <Ishanvi.41238@stu.upes.ac.in>
2026-09-17 16:49:43 +0530

    Add program to calculate the sum of two numbers

---
+++ Day01/q1.c
@@ -0,0 +1,22 @@
+/*
+Name: Ishanvi Pahil
+SAP ID: 590041238
+Day: 01
+Question: 01
+Date: 01-09-2026
+Q1: WAP to input two numbers and display their sum
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d", a + b);
+
+    return 0;
+}
