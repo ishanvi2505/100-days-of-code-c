9f4fc22331f8f8ebd78d88dba8165c6242ee7011
ishanvi2505 <Ishanvi.41238@stu.upes.ac.in>
2026-09-24 23:47:57 +0530

    Add main function to calculate series sum

---
+++ Day22/q44.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (float)(2 * i - 1) / (2 * i);
+    }
+
+    printf("%f", sum);
+
+    return 0;
+}
